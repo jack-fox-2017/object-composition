b0da795d6d85da689aa7d65b06763b0ac0d673fd
anto <irianto223@gmail.com>
2017-08-01 14:44:13 +0700

    class Ingredient DONE

--- composition.js
+++ composition.js
@@ -1,8 +1,8 @@
 class Cookie {
-  constructor(name) {
+  constructor(name, ingredients) {
     this.name = name
     this.status = 'mentah'
-    this.ingredients = []
+    this.ingredients = ingredients
   }
 
   bake() {
@@ -10,52 +10,101 @@ class Cookie {
   }
 }
 
+
 class PeanutButter extends Cookie {
-  constructor(name) {
+  constructor(name, ingredients) {
     super()
     this.peanut_count = 100
     this.name = name
+    this.ingredients = ingredients
   }
 }
 
+
 class ChocolateChip extends Cookie {
-  constructor(name) {
+  constructor(name, ingredients) {
     super()
     this.choco_chip_count = 200
     this.name = name
+    this.ingredients = ingredients
   }
 }
 
+
 class OtherCookie extends Cookie {
-  constructor(name) {
+  constructor(name, ingredients) {
     super()
     this.other_count = 150
     this.name = name
+    this.ingredients = ingredients
   }
 }
+
 
 class CookieFactory {
   constructor() {}
 
   static create(options) {
     let arr = []
-    for (let i=0; i<list.length; i++) {
-      if (list[i] == 'peanut butter') {
-        arr.push(new PeanutButter(list[i]))
+    for (let i=0; i<options.length; i++) {
+      if (options[i].split(' = ')[0] == 'peanut butter') {
+        arr.push(new PeanutButter(options[i].split(' = ')[0], this.olahIngredients(options[i].split(' = ')[1])))
       }
-      else if (list[i] == 'chocolate chip') {
-        arr.push(new ChocolateChip(list[i]))
+      else if (options[i].split(' = ')[0] == 'chocolate chip') {
+        arr.push(new ChocolateChip(options[i].split(' = ')[0], this.olahIngredients(options[i].split(' = ')[1])))
       }
       else {
-        arr.push(new OtherCookie(list[i]))
+        arr.push(new OtherCookie(options[i].split(' = ')[0], this.olahIngredients(options[i].split(' = ')[1])))
       }
     }
     return arr
   }
+
+  static olahIngredients(strBahan) {
+    let arr = []
+    let perBahan = strBahan.split(', ')
+    for (var i = 0; i < perBahan.length; i++) {
+      let obj = {}
+      let amountName = perBahan[i].split(' : ')
+      obj.name = amountName[1]
+      obj.amount = amountName[0]
+      if (obj.name == 'sugar') {
+        obj.has_sugar = true
+      }
+      else {
+        obj.has_sugar = false
+      }
+      arr.push(JSON.stringify(new Ingredient(obj)))
+    }
+    return arr
+  }
+
+  static cookieRecomendation() {
+    let today = 'tuesday'
+    // let today = 'sunday'
+    if (today == 'tuesday') {
+      // 
+    }
+    else {
+      //
+    }
+  }
 }
 
 
+class Ingredient {
+  constructor(options) {
+    this.name = options['name']
+    this.amount = options['amount']
+    this.has_sugar = options['has_sugar']
+  }
+}
+
+
+
+
+
 let fs = require('fs')
-let list = fs.readFileSync('cookies.txt', 'utf8').trim().split('\n')
+let list = fs.readFileSync('cookies2.txt', 'utf8').trim().split('\n')
 
 console.log(CookieFactory.create(list));
