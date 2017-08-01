4348a8f10f6dd50238effa2ac43fe7aac63abcc1
ahmadaidil <ahmdaidil@gmail.com>
2017-08-01 17:51:15 +0700

    final

--- composition.js
+++ composition.js
@@ -2,42 +2,31 @@ const fs = require('fs');
 
 class Ingredient{
   constructor(options){
-    this.name = this.ing(options);
-    this.amount = this.amo(options);
-    this.hasSugar = this.sugar();
-  }
-  ing(options){
-    let ingr = [];
-    let ingSplit = options[1].toString().split(',');
-    for(let i=0; i<ingSplit.length; i++){
-      ingr.push(ingSplit[i].toString().split(':')[1].trim());
-    }
-    return ingr.join(', ');
-  }
-  amo(options){
-    let amount = [];
-    let ingSplit = options[1].toString().split(',');
-    for(let i=0; i<ingSplit.length; i++){
-      amount.push(ingSplit[i].toString().split(':')[0].trim());
-    }
-    return amount.join(', ');
-  }
-  sugar(){
-    let ingName = this.name.trim().split(',');
-    for(let i=0; i<ingName.length; i++){
-      if(ingName[i].trim()==='sugar'){
-        return true;
-      }
-    }
-    return false;
+    this.name = options.name;
+    this.amount = options.amount;
+    this.hasSugar = options.hasSugar;
   }
 }
 
 class Cookie{
-  constructor(options){
-    this.name = options[0];
+  constructor(ingredient){
+    this.name = ingredient[0];
     this.status = 'mentah';
-    this.ingredient = new Ingredient(options);
+    this.ingredient = this.ing(ingredient);
+  }
+  ing(ingredient){
+    let sp = ingredient[1].toString().split(',');
+    let ingr = [];
+    for(let i=0; i<sp.length; i++){
+      let spl = sp[i].toString().split(':');
+      let sugar = false;
+      if(spl[1].trim()=='sugar'){
+        sugar = true;
+      }
+      let options = {name: spl[1].trim(), amount: spl[0].trim(), hasSugar:sugar}
+      ingr.push(new Ingredient(options));
+    }
+    return ingr;
   }
   bake(){
     this.status = 'selesai dimasak';
@@ -45,22 +34,22 @@ class Cookie{
 }
 
 class PeanutButter extends Cookie{
-  constructor(name){
-    super(name);
+  constructor(opsi){
+    super(opsi);
     this.peanut_count = 100;
   }
 }
 
 class ChocholateChip extends Cookie{
-  constructor(name){
-    super(name);
+  constructor(opsi){
+    super(opsi);
     this.chocChip_count = 200;
   }
 }
 
 class OtherCookie extends Cookie{
-  constructor(name){
-    super(name);
+  constructor(opsi){
+    super(opsi);
     this.other_count = 150;
   }
 }
@@ -88,7 +77,13 @@ class CookieFactory{
   static recommend(day, batch){
     let recomm = [];
     for(let i=0; i<batch.length; i++){
-      if(!batch[i].ingredient.hasSugar){
+      let count = 0;
+      for(let j=0; j<batch[i].ingredient.length; j++){
+        if(!batch[i].ingredient[j].hasSugar){
+          count++;
+        }
+      }
+      if(count == batch[i].ingredient.length){
         recomm.push(batch[i]);
       }
     }
@@ -99,9 +94,10 @@ class CookieFactory{
 let options = fs.readFileSync('cookies.txt', 'utf8').toString().trim().split('\n');
 let batchOfCookies = CookieFactory.create(options);
 console.log(batchOfCookies);
+
 let sugarFreeFoods = CookieFactory.recommend('tuesday', batchOfCookies);
 console.log('\nsugar free cakes are :');
 for(let i=0; i<sugarFreeFoods.length; i++){
   console.log(sugarFreeFoods[i].name);
-  console.log('because the ingredients is',sugarFreeFoods[i].ingredient.name);
+  console.log('\nbecause the ingredients is\n',sugarFreeFoods[i].ingredient);
 }
